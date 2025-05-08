b2c9b04bd739edb4f716cb251bf298f8f61aaeaf
mtzshop <pedroyoanmd@gmail.com>
2025-05-08 09:37:27 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,css,js,ts,jsx,tsx}", 
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+            serifcustom: 'Lora',
+            inter: 'Inter',
+            strike: 'Protest Strike'
+      },
+      colors: {
+        'malachite': {
+            '50': '#e7ffe4',
+            '100': '#c9ffc5',
+            '200': '#97ff92',
+            '300': '#56ff53',
+            '400': '#20fb23',
+            '500': '#00de07',
+            '600': '#00b50b',
+            '700': '#02890b',
+            '800': '#086c10',
+            '900': '#0c5b13',
+            '950': '#003306',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
